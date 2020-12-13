53c79f44265b98c96f5ffb56588611cab6ad4c23
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2020-12-13 16:27:36 +0900

    データ送信、保存処理

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	get 'home' => 'home#index'
 
 	namespace :api, format: 'json' do
-		resources :memos, only: [:index]
+		resources :memos, only: [:index, :create]
 	end
 
 end
